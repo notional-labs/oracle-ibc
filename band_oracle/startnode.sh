6bc7a57fdfaafebb49a1352f254a64b69ae34996
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2022-02-06 10:02:35 +0700

    minor

--- band_oracle/startnode.sh
+++ band_oracle/startnode.sh
@@ -8,7 +8,7 @@ rm -rf $HOME/.band
 cd $HOME
 
 bandd init testing --chain-id=$CHAIN_ID --home=$HOME/.band
-bandd keys add $WALLET_NAME --home=$HOME/.band
+bandd keys add $WALLET_NAME --home=$HOME/.band --recover
 bandd add-genesis-account $(bandd keys show $WALLET_NAME -a --home=$HOME/.band) 1000000000uband --home=$HOME/.band
 bandd gentx $WALLET_NAME 500000000uband --home=$HOME/.band --chain-id=$CHAIN_ID
 bandd collect-gentxs --home=$HOME/.band
